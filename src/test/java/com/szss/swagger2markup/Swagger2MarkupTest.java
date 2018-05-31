fb6164957015840488a15c356e6d98bd404b62b8
鼠笑天 <zhuchuangang20@163.com>
2018-05-31 10:59:59 +0800

    修改跳过测试打包失败的情况

--- src/test/java/com/szss/swagger2markup/Swagger2MarkupTest.java
+++ src/test/java/com/szss/swagger2markup/Swagger2MarkupTest.java
@@ -21,11 +21,6 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
-// import io.github.robwin.markup.builder.MarkupLanguage;
-// import io.github.robwin.swagger2markup.GroupBy;
-// import io.github.robwin.swagger2markup.Swagger2MarkupConverter;
-// import springfox.documentation.staticdocs.SwaggerResultHandler;
-
 @WebAppConfiguration
 @RunWith(SpringJUnit4ClassRunner.class)
 @AutoConfigureRestDocs(outputDir = "build/asciidoc/snippets")
@@ -38,27 +33,10 @@ public class Swagger2MarkupTest {
 
     private String snippetDir = "target/generated-snippets";
     private String outputDir = "target/swagger2";
-    // private String indexDoc = "docs/asciidoc/index.adoc";
-
-    // @Test
-    // public void test() throws Exception {
-    // // 得到swagger.json,写入outputDir目录中
-    // mockMvc.perform(RestDocumentationRequestBuilders.get("/v2/api-docs").accept(MediaType.APPLICATION_JSON))
-    // .andDo(SwaggerResultHandler.outputDirectory(outputDir).build()).andExpect(status().isOk()).andReturn();
-    //
-    // // 读取上一步生成的swagger.json转成asciiDoc,写入到outputDir
-    // // 这个outputDir必须和插件里面<generated></generated>标签配置一致
-    // Swagger2MarkupConverter.from(outputDir + "/swagger.json");
-    //// .withPathsGroupedBy(GroupBy.TAGS)// 按tag排序
-    //// .withMarkupLanguage(MarkupLanguage.ASCIIDOC)// 格式
-    //// .withExamples(snippetDir).build().intoFolder(outputDir);// 输出
-    // }
+    private String indexDoc = "target/asciidoc";
 
     @Test
     public void createSpringfoxSwaggerJson() throws Exception {
-        // String designFirstSwaggerLocation = Swagger2MarkupTest.class.getResource("/swagger.yaml").getPath();
-
-        // String outputDir = System.getProperty("io.springfox.staticdocs.outputDir");
         MvcResult mvcResult = this.mockMvc.perform(get("/v2/api-docs").accept(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk()).andReturn();
 
@@ -70,4 +48,5 @@ public class Swagger2MarkupTest {
             writer.write(swaggerJson);
         }
     }
+
 }
